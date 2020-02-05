e16e6357a7aba3213d4210af3bcddd638979f38b
Jonathan Cervantes <joncerv@Jonathans-MacBook-Pro.local>
2020-02-04 16:28:19 -0800

    got rid of api key

--- src/yelp.js
+++ src/yelp.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import axios from 'axios'
 
-const REACT_APP_YELP = 'awOJFMrLfZ0QhRWqvREwwby0E3r8vqEXP5F-FYb25tbulDZI9LW2Fknk7eXNDhaWTnuUeUm-C3R4bJS6rsGSQEzxHoPchsZXq06oVfKvj7mOopv8VgJ-FmsumhA2XnYx'
+
 const api = axios.create({
   baseURL: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3',
   headers: {
-   Authorization: `Bearer ${REACT_APP_YELP}`
+   Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`
  },
 })
 const getGasStations = userLocation => {
   return api.get('businesses/search',{
       params: {
         limit: 10,
-        categories: 'coffee',
+        categories: 'gas',
         latitude: userLocation.lat,
         longitude: userLocation.long,
       }
